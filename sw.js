dedc815ba734ecb480e9d09783a78bf45782d7e1
manacup <91571517+manacup@users.noreply.github.com>
2021-11-09 16:58:28 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,17 @@
-const cacheName = 'ManaCup-v1';
-// Installing Service Worker
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(contentToCache);
-  })());
-});
-
-// Fetching content using Service Worker
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) return r;
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
-});
+    e.waitUntil(
+      caches.open('manacup').then((cache) => cache.addAll([
+          '/',
+        '/app.js',
+        '/manacup2122.html',
+        '/style.css'
+      ])),
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+  });
