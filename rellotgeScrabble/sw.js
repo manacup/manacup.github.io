eaa3ccbd72714dd87fb8ff65db4e17b2c9ab4712
manacup <91571517+manacup@users.noreply.github.com>
2021-11-09 17:27:53 +0100

    Add files via upload

---
+++ rellotgeScrabble/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('rellotgeScrabble').then((cache) => cache.addAll([
+          '/rellotgeScrabble/',
+        '/rellotgeScrabble/js/script.js',
+        '/rellotgeScrabble/index.html',
+        '/rellotgeScrabble/css/style.css'
+      ])),
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+  });
