a7e16846ee5e87e43c97dca92bbc7eb4041ca5d2
manacup <91571517+manacup@users.noreply.github.com>
2023-09-28 09:20:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,23 @@
 const versio = "2023-24.0"
-self.addEventListener('install', (e) => {
+/* self.addEventListener('install', (e) => {
     e.waitUntil(
-      caches.open('manacup').then((cache) => cache.addAll([
+      caches.open('manacup24').then((cache) => cache.addAll([
          
       ])),
     );
-  });
+  }); 
+
   
   self.addEventListener('fetch', (e) => {
     console.log(e.request.url);
     e.respondWith(
       caches.match(e.request).then((response) => response || fetch(e.request)),
     );
+  });*/
+if ('serviceWorker' in navigator) {
+  caches.keys().then(function(cacheNames) {
+    cacheNames.forEach(function(cacheName) {
+      caches.delete(cacheName);
+    });
   });
+}
