a16a494fd226f626ff51dec4ce748a5ebe5dd4f1
manacup <91571517+manacup@users.noreply.github.com>
2023-11-20 18:43:52 +0000

    f

--- provaMAnacupJSON/imatgeconf.js
+++ provaMAnacupJSON/imatgeconf.js
@@ -58,7 +58,7 @@ var carregaImatgeConf = function (event) {
   reader3.onload = function () {
     /* var output3 = document.getElementById("resjugImatgeConf");
     output3.src = reader3.result; */
-    dades.filter((j) => j.Nom == nom)[0].Imatge = reader3.result
+    //dades.filter((j) => j.Nom == nom)[0].Imatge = reader3.result
     jugadorDesat.Imatge = reader3.result
     document.querySelectorAll(".userImg").forEach(ui=>{
         ui.src = reader3.result
