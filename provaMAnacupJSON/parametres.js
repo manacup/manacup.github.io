dc1de891e61f42c3f8603f47bb951cefbb92bb6a
manacup <91571517+manacup@users.noreply.github.com>
2023-11-20 10:01:58 +0100

    Update parametres.js

--- provaMAnacupJSON/parametres.js
+++ provaMAnacupJSON/parametres.js
@@ -76,7 +76,9 @@ function iniciJSON() {
   fetch(
     "https://script.google.com/macros/s/AKfycbwDcFyPQFV3B0bzeRxGU9yaTWhbA3PyR3SQZOQ1KEE5cU08SJb5QaOOfuXxwfVnuASk/exec?page=JSON"
   )
-    .then((response) => response.json())
+    .then((response) => {
+        response.json()
+        console.log(response)})
     .then((data) => {
       console.log(data);
       dades = data.dades;
