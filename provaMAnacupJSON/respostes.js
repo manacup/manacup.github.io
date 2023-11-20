13bd4a697138aef9cee320a57ac58dbd4382be62
manacup <91571517+manacup@users.noreply.github.com>
2023-11-20 11:54:38 +0000

    z

--- provaMAnacupJSON/respostes.js
+++ provaMAnacupJSON/respostes.js
@@ -337,7 +337,7 @@ async function handleFormSubmit(formObject) {
   .then(response => response.text())
   .then(data => {
     console.log('Resposta del servidor:', data);
-    setTimeout(funcioInici("classificacions"), 2000)
+    setTimeout(iniciJSON("classificacions"), 2000)
   })
   .catch(error => console.error('Error:', error));
   /* google.script.run
