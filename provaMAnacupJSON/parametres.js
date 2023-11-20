844e3e12bd04f344df3855188849d1410f64489b
manacup <91571517+manacup@users.noreply.github.com>
2023-11-20 10:18:19 +0100

    Update parametres.js

--- provaMAnacupJSON/parametres.js
+++ provaMAnacupJSON/parametres.js
@@ -1,6 +1,6 @@
 const queryString = window.location.search;
 console.log(queryString);
-const urlParams = new URLSearchParams(queryString);
+const urlParams = Object.fromEntries(new URLSearchParams(queryString));
 console.log(urlParams);
 
 
