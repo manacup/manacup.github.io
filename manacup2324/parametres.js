3e2edd7ecde42a7f226373c66c118354c0a3ad15
manacup <91571517+manacup@users.noreply.github.com>
2023-11-22 17:10:55 +0000

    s

--- manacup2324/parametres.js
+++ manacup2324/parametres.js
@@ -7,8 +7,11 @@ let parameterVista = urlParams.vista
 let parameterOptions = urlParams.options
 let vistaPredet = { page: parameterVista, options: parameterOptions };
 let idfull = urlParams.idfull
-let pestanyes =  urlParams.pestanyes || ""
-  document.getElementById("collapsetabs").classList.add(pestanyes)
+let mostrapestanyes =  urlParams.mostrapestanyes 
+  if(mostrapestanyes="si"){
+    document.getElementById("collapsetabs").classList.add("show")
+    document.getElementById("pestanyes").checked=true
+  }
 const imatgeFixa =
   "https://www.infobae.com/new-resizer/izGq0GB3EIUlIN4fdPOhc_rT54c=/arc-anglerfish-arc2-prod-infobae/public/IPZBXHKPUJAOVHO662LV25OEOM.jpg";
 let dades = [];
