695fe7c61a076ec1dccd0e306876e3b405b19887
manacup <91571517+manacup@users.noreply.github.com>
2023-12-12 03:45:01 +0100

    Add files via upload

--- anagrama/src/index.js
+++ anagrama/src/index.js
@@ -48,28 +48,80 @@ const DnD = {
   }
   
 };
+const randomNumsArray = ["","","","","","",""];
 
-const randomNums = () => {
-  const array = ["","","","","","","","a","b","a","v","e","i","r",];
-  let i = array.length,
-    j = 0,
-    temp;
-  while (i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+function obtenerFichasAleatorias(distribucion) {
+  const letrasDisponibles = distribucion
+
+  // Función auxiliar para obtener un elemento aleatorio y actualizar la distribución
+  function obtenerLetraAleatoria() {
+    const letra = letrasDisponibles[Math.floor(Math.random() * letrasDisponibles.length)];
+    //distribucion[letra].cantidad--;
+
+    // Eliminar letras agotadas de la lista
+  /*   if (distribucion[letra].cantidad === 0) {
+      const index = letrasDisponibles.indexOf(letra);
+      letrasDisponibles.splice(index, 1);
+    }*/
+
+    return letra; 
   }
-  return array;
+
+  const fichasAleatorias = randomNumsArray;
+  for (let i = 0; i < 7; i++) {
+    const letraAleatoria = obtenerLetraAleatoria();
+    fichasAleatorias.push(letraAleatoria);
+  }
+
+  return fichasAleatorias;
+}
+
+// Ejemplo de uso con la distribución de Scrabble en catalán
+const distribucionScrabbleCatala = {
+  'A': { cantidad: 12, valor: 1 },
+  'B': { cantidad: 2, valor: 3 },
+  'C': { cantidad: 3, valor: 3 },
+  'D': { cantidad: 3, valor: 2 },
+  'E': { cantidad: 13, valor: 1 },
+  'F': { cantidad: 1, valor: 4 },
+  'G': { cantidad: 2, valor: 2 },
+  'H': { cantidad: 1, valor: 4 },
+  'I': { cantidad: 8, valor: 1 },
+  'J': { cantidad: 1, valor: 8 },
+  'L': { cantidad: 4, valor: 1 },
+  'M': { cantidad: 3, valor: 3 },
+  'N': { cantidad: 6, valor: 1 },
+  'O': { cantidad: 5, valor: 1 },
+  'P': { cantidad: 2, valor: 3 },
+  'Qu': { cantidad: 1, valor: 8 },
+  'R': { cantidad: 8, valor: 1 },
+  'S': { cantidad: 8, valor: 1 },
+  'T': { cantidad: 5, valor: 1 },
+  'U': { cantidad: 4, valor: 1 },
+  'V': { cantidad: 1, valor: 4 },
+  'X': { cantidad: 1, valor: 8 },
+  
+  'Z': { cantidad: 1, valor: 10 },
+  'Ç': { cantidad: 1, valor: 4 },
+  'L·L': { cantidad: 1, valor: 8 },
+  'NY': { cantidad: 1, valor: 8 },
+  
 };
-const randomNumsArray = ["","","","","","","","a","b","a","v","e","i","r",];
+const arrayRepetido = Object.entries(distribucionScrabbleCatala)
+  .flatMap(([letra, { cantidad }]) => Array(cantidad).fill(letra));
+
+console.log(arrayRepetido);
+
+let fichasAleatorias = obtenerFichasAleatorias(arrayRepetido);
+console.log(fichasAleatorias);
+
 
 
 
 const boxes = document.querySelectorAll(".grid-item[draggable]");
 function inici(){
   [].forEach.call(boxes, function(box, index) {
-    box.textContent = randomNumsArray[index]; })
+    box.textContent = fichasAleatorias[index]; })
 }
 
 
@@ -98,3 +150,26 @@ function updateWordInput() {
 
   wordInput.value = word;
 }
+/* document.addEventListener("DOMContentLoaded", function() {
+  const miDiv = document.getElementById("rack");
+  const elementos = Array.from(miDiv.querySelectorAll(".grid-item"));
+
+  // Función para reordenar aleatoriamente un array
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  // Reordenar aleatoriamente los elementos
+  shuffleArray(elementos);
+
+  // Vaciar el div
+  miDiv.innerHTML = "";
+
+  // Agregar los elementos reordenados al div
+  elementos.forEach(function(elemento) {
+    miDiv.appendChild(elemento);
+  });
+}); */
