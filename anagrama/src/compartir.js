fe4ad00076e12ddce76deed0c990ea960afccda5
manacup <91571517+manacup@users.noreply.github.com>
2023-12-14 01:39:47 +0100

    Add files via upload

---
+++ anagrama/src/compartir.js
@@ -0,0 +1,27 @@
+let urlactual = window.location.href
+function toggleBotons() {
+  var botonsCompartir = document.getElementById('botonscompartir');
+  botonsCompartir.classList.toggle('mostrar');
+}
+function compartirWhatsapp() {
+    // URL que vols compartir
+
+    var enllac = encodeURIComponent(urlactual+"?faristol="+nouFaristol.join("-"));
+
+    // Crea l'enllaç d'invocació de WhatsApp
+    var urlWhatsapp = 'https://wa.me/?text=He aconseguit trobar '+paraulesUsades.length+' jugades. Ho pots superar? ' + enllac;
+
+    // Obre una nova finestra o pestanya amb l'enllaç de WhatsApp
+    window.open(urlWhatsapp, '_blank');
+  }
+
+  function compartirTelegram() {
+    // URL que vols compartir
+    var enllac = encodeURIComponent(urlactual+"?faristol="+nouFaristol.join("-"));
+
+    // Crea l'enllaç d'invocació de Telegram
+    var urlTelegram = 'https://t.me/share/url?url=' + enllac;
+
+    // Obre una nova finestra o pestanya amb l'enllaç de Telegram
+    window.open(urlTelegram, '_blank');
+  }
