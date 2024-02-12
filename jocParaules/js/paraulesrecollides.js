44d524ddde602b0fff27524888abc3c3c087b649
manacup <91571517+manacup@users.noreply.github.com>
2024-02-12 19:09:58 +0100

    Update paraulesrecollides.js

--- jocParaules/js/paraulesrecollides.js
+++ jocParaules/js/paraulesrecollides.js
@@ -1,12 +1,15 @@
 
+var paraulesConsultades
+const url = "https://script.google.com/macros/s/AKfycbzN6ZwJ3q46WzSnrHNg053WcXtsGxsGyuakqg1dbTg4IKTEvS_bqwhe408tbOLYwBjE/exec";
+
 async function fetchJSON(url) {
   const response = await fetch(url);
   const jsonData = await response.json();
-  const constantValue = jsonData.paraules; // Selecciona el camp `paraules` del JSON
-  return constantValue;
+  paraulesConsultades = jsonData.paraules; // Selecciona el camp `paraules` del JSON
+  return 
 }
-const url = "https://script.google.com/macros/s/AKfycbzN6ZwJ3q46WzSnrHNg053WcXtsGxsGyuakqg1dbTg4IKTEvS_bqwhe408tbOLYwBjE/exec";
 
-const paraulesConsultades = await fetchJSON(url);
 
-console.log(paraules); // Mostra el valor del camp `paraules`
+
+
+
