03db83a6e267c2fa48b7880bcdebe46368c61eaa
manacup <91571517+manacup@users.noreply.github.com>
2024-04-10 13:49:44 +0200

    Update script.js

--- comparativa/script.js
+++ comparativa/script.js
@@ -25,7 +25,7 @@ document.getElementById("resposta").innerHTML+=campionattemplate
 
 
 const partidatemplate = `
-<li class="list-group-item d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#partidaModal">${partida.jugador} (${partida.puntsJugador}-${partida.puntsAdversari}) ${partida.adversari}</li>
+<li class="list-group-item d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#partidaModal" onclick="recuperaPartida(${partida.campionat},${partida.row})">${partida.jugador} (${partida.puntsJugador}-${partida.puntsAdversari}) ${partida.adversari}</li>
 `
 document.getElementById("resposta").innerHTML+=partidatemplate
 
@@ -105,6 +105,7 @@ var adversari=encodeURIComponent(adversari)
   }
 }
 function recuperaPartida(full,row){
+    spinner("")
    // Crida a l'API del Google Apps Script
   var myHeaders = new Headers();
   var myInit = {
@@ -129,5 +130,140 @@ mostraPartida(partida)
 }   
 function mostraPartida(partida){
 var body = document.getElementById("partidaModalBody")
+
+  const partidaTemplate = `
     
+      <div class="p-1">
+        <div class="d-flex align-items-center p-2">
+          <h1 class="flex-grow-1">Ronda ${partida.Ronda}</h1>
+          <div class=" position-relative me-4" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Jugada a trobada oficial">
+            <span class="h1 material-symbols-outlined">diversity_3</span>
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success ${
+              partida.Punts_social == 0 ? "d-none" : ""
+            }"><i class="bi bi-check-lg"></i></span>
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger ${
+              partida.Punts_social == 1 ? "d-none" : ""
+            }"><i class="bi bi-x-lg"></i></span>
+          </div>
+          <div class=" position-relative me-4" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Cuitor en jugar la partida">
+            <span class="h1 material-symbols-outlined">speed</span>
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary">+${
+              partida.Punts_velocitat
+            }</span>
+          </div>
+        </div>
+        <div class="card mb-4 ">
+          <div class="text-center position-relative">
+          <a class="position-absolute top-10 end-0 translate-middle-x pt-2 pe-1" href="${
+            urlApp + "&vista=detallpartida&options=" + partida.ID
+          }" target="_blank" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Enllaç a la vista actual">
+                    <i class="bi bi-share-fill"></i>
+                  </a>
+            <div class="d-flex justify-content-center">
+              <div class="input-group mt-4 d-flex justify-content-center">
+                <span class="input-group-text ${
+                  partida.Puntuacio_1 > partida.Puntuacio_2
+                    ? "bg-primary text-white"
+                    : ""
+                }">
+                <span class="display-1">${partida.Puntuacio_1}</span>
+                </span>
+                <span class=" input-group-text  ${
+                  partida.Puntuacio_2 > partida.Puntuacio_1
+                    ? "bg-primary text-white"
+                    : ""
+                }">
+                <span class="display-1">${partida.Puntuacio_2}</span>
+                </span>
+              </div>
+            </div>
+            <div class="text-muted mb-3">Suma: ${partida.Suma_punts}</div>
+            <div class="d-flex justify-content-center mb-4">
+              <div class="col align-items-start">
+                <h4 class="m-2 nom">${partida.Jugador1}</h4>
+                <div class="d-flex flex-column align-items-center">
+                  <div class="card m-2 p-2 w-75 bg-primary bg-opacity-25 rounded-4">
+                    <small>Millor jugada</small>
+                    <h6 class="m-2 mot">${partida.Mot_1}</h6>
+                    <h4>${partida.Puntsmot_1}</h4>
+                    <small class="text-muted">Punts</small>
+                  </div>
+                  <div class="card m-2 p-2 w-75 bg-primary bg-opacity-25 rounded-4">
+                    <small>Scrabbles</small>
+                    <h4>${partida.Scrabbles_1}</h4>
+                  </div>
+                </div>
+              </div>
+              <div class="vr"></div>
+              <div class="col align-items-end">
+                <h4 class="m-2 nom">${partida.Jugador2}</h4>
+                <div class="d-flex flex-column align-items-center">
+                  <div class="card m-2 p-2 w-75 bg-primary bg-opacity-25 rounded-4">
+                    <small>Millor jugada</small>
+                    <h6 class="m-2 mot">${partida.Mot_2}</h6>
+                    <h4>${partida.Puntsmot_2}</h4>
+                    <small class="text-muted">Punts</small>
+                  </div>
+                  <div class="card m-2 p-2 w-75 bg-primary bg-opacity-25 rounded-4">
+                    <small>Scrabbles</small>
+                    <h4>${partida.Scrabbles_2}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="carouselExampleControls" class="carousel slide mb-4" data-bs-ride="carousel">
+          <div class="carousel-inner" >
+            <div class="carousel-item active quadrat">
+              <img src="${
+                partida.Full || imatgeFixa
+              }" class="d-block  rounded cover img-fluid" onclick="loadContent(['imatge',this])">
+            </div>
+            <div class="carousel-item quadrat">
+              <img src="${
+                partida.Tauler || imatgeFixa
+              }" class="d-block rounded cover img-fluid" onclick="loadContent(['imatge',this])">
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+    <p>Resultats enviats el ${ExcelDateToJSDate(partida.Data)}</p>
+        <blockquote class="blockquote bg-danger bg-opacity-25 mb-4" style="border-left: 2px solid red;">
+          <div class="mb-0 p-3 ${!partida.Comentaris ? "d-none" : ""}">${
+    partida.Comentaris
+  }</div>
+        </blockquote>
+   
+      </div>
+          
+  `;
+ 
+  body.innerHTML = partidaTemplate;
+ spinner("d-none")
+}
+function ExcelDateToJSDate(serial) {
+   var utc_days  = Math.floor(serial - 25569);
+   var utc_value = utc_days * 86400;                                        
+   var date_info = new Date(utc_value * 1000);
+
+   var fractional_day = serial - Math.floor(serial) + 0.0000001;
+
+   var total_seconds = Math.floor(86400 * fractional_day);
+
+   var seconds = total_seconds % 60;
+
+   total_seconds -= seconds;
+
+   var hours = Math.floor(total_seconds / (60 * 60));
+   var minutes = Math.floor(total_seconds / 60) % 60;
+var jsdate = new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds)
+   return jsdate.toLocaleDateString();
 }
