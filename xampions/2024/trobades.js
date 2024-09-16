04dde74acde7724af5d76661632234e8a9802551
manacup <91571517+manacup@users.noreply.github.com>
2024-09-16 14:45:39 +0200

    Update trobades.js

--- xampions/2024/trobades.js
+++ xampions/2024/trobades.js
@@ -339,7 +339,7 @@ function renderFormTrobada(trobada) {
         <div class="col-12">
           <div class="input-group mb-3">
             
-            <input type="search" class="form-control llistajugadorsform" name="Nom" id="jugAssistencia" required disabled  value="${
+            <input type="search" class="form-control llistajugadorsform" name="Nom" id="jugAssistencia" required readonly  value="${
               jugadorDesat.Nom || ""
             }" placeholder="Comença a escriure el nom i tria de la llista" >
              <button class="btn btn-primary" type="button" id="button-addon2" data-bs-toggle="modal" data-bs-target="#desajug" aria-expanded="false"
