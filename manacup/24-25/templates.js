c407df5e36cf867e5847cbafa25436c0262c5cf0
manacup <clubscrabblemanacor@gmail.com>
2024-09-30 23:23:49 +0200

    ordenades

--- manacup/24-25/templates.js
+++ manacup/24-25/templates.js
@@ -543,7 +543,7 @@ function renderJugador(jugador) {
   });
   // Agregar evento al botón de ordenar
 
-  var ordenada = false;
+  var ordenada = true;
   document.getElementById("ordenarBoton").addEventListener("click", () => {
     // Llama a la función de ordenar la tabla por la segunda columna (Edad)
     if (!ordenada) {
