ed5aa13658b45e9e2e53feb40640853540ff0b45
manacup <91571517+manacup@users.noreply.github.com>
2024-10-14 22:33:23 +0200

    Update parametres.js

--- manacup/24-25/parametres.js
+++ manacup/24-25/parametres.js
@@ -58,7 +58,7 @@ function carregaUsuari() {
 document.addEventListener("DOMContentLoaded", iniciJSON(false));
 
 function iniciJSON(turbo,vista) {
-  usaJSONfixe?turbo=true:""
+  usaJSONfixe="true"?turbo=true:""
   carregant();
   carrega = 0;
   // Crida a l'API del Google Apps Script
