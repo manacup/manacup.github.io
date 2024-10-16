7af075cffa8936be8c245ab2933dac2277ec6ea1
manacup <91571517+manacup@users.noreply.github.com>
2024-10-16 10:11:17 +0200

    Update detallJugadorCtrl.js

--- angular/js/controllers/detallJugadorCtrl.js
+++ angular/js/controllers/detallJugadorCtrl.js
@@ -2,6 +2,9 @@ app.controller("detallJugadorCtrl", function($scope, $rootScope,$routeParams) {
     $rootScope.pagina = "Detall";
   var vm = this;
     vm.id = $routeParams.id;
+    console.log(vm.id)
+    console.log($scope.aparellaments)
+     console.log($scope.jugadors)
     $scope.resumJugador = $scope.jugadors[vm.id]
     $scope.partidesJugador = $scope.aparellaments.filter((ap)=>{ap.Jugador1.ID == vm.id && ap.Jugador2.ID == vm.id})
 });
