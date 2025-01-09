9b12f7a439b384fcec0cc2c6cbab1de592040343
manacup <91571517+manacup@users.noreply.github.com>
2025-01-09 12:31:15 +0100

    d

--- angular/js/app.js
+++ angular/js/app.js
@@ -54,10 +54,8 @@ app.config(function($routeProvider) {
             controllerAs: 'vm',
             resolve: {
               // I will cause a 1 second delay
-              delay: function($q, $timeout) {
-                var delay = $q.defer();
-                $timeout(delay.resolve, 1000);
-                return delay.promise;
+              dades: function() {                
+                return 
               }
             }
         });
