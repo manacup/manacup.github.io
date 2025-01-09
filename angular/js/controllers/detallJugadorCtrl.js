6330c0ae40d55b03e0bbe9fa626ff569a4a321ee
manacup <91571517+manacup@users.noreply.github.com>
2025-01-09 13:49:32 +0100

    d

--- angular/js/controllers/detallJugadorCtrl.js
+++ angular/js/controllers/detallJugadorCtrl.js
@@ -7,8 +7,8 @@ app.controller("detallJugadorCtrl", function ($scope, $rootScope, $routeParams) 
   $scope.resumJugador = $scope.jugadors[vm.id]
   $scope.partidesJugador = Object.values($scope.aparellaments).filter(ap => ap.Jugador1.ID == vm.id || ap.Jugador2.ID == vm.id)
   $scope.partidesJugador.forEach((partidaJug)=>{
-    partidaJug.jugadorObj = partidaJug.Jugador1.ID == vm.id? partidaJug.Jugador1 : partidaJug.Jugador2
-    partidaJug.jugadorAdv = partidaJug.Jugador1.ID != vm.id? partidaJug.Jugador1 : partidaJug.Jugador2
+    partidaJug.JugadorObj = partidaJug.Jugador1.ID == vm.id? partidaJug.Jugador1 : partidaJug.Jugador2
+    partidaJug.JugadorAdv = partidaJug.Jugador1.ID != vm.id? partidaJug.Jugador1 : partidaJug.Jugador2
 })
   $scope.campionats = $scope.resumJugador.Campionats.split(/\n/);
   $scope.podis = $scope.resumJugador.Podis.split(/\n/);
