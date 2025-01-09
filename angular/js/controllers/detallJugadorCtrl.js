bbd725e1fc91ead3edfd1b1c1eb72d6fc395b339
manacup <91571517+manacup@users.noreply.github.com>
2025-01-09 11:11:17 +0100

    s

--- angular/js/controllers/detallJugadorCtrl.js
+++ angular/js/controllers/detallJugadorCtrl.js
@@ -6,10 +6,9 @@ app.controller("detallJugadorCtrl", function ($scope, $rootScope, $routeParams) 
   console.log('iniciant dades del jugador');
   $scope.resumJugador = $scope.jugadors[vm.id]
   $scope.partidesJugador = Object.values($scope.aparellaments).filter(ap => ap.Jugador1.ID == vm.id || ap.Jugador2.ID == vm.id)
-  $scope.campionats = jugador.Campionats.split(/\n/).join(", ") || "Encara no ha guanyat cap campionat.";
-  $scope.podis = jugador.Podis.split(/\n/).join(", ") || "Encara no ha fet cap podi.";
-  $scope.diplomes = jugador.Diplomes.split(/\n/).join(", ") ||
-  "Encara no ha aconseguit cap diploma olímpic.";
+  $scope.campionats = jugador.Campionats.split(/\n/);
+  $scope.podis = jugador.Podis.split(/\n/);
+  $scope.diplomes = jugador.Diplomes.split(/\n/);
   $scope.posConjuntaArray = jugador.partides
     .map((p) => parseInt(p.Pos_Conjunta))
     .filter((pa) => !Number.isNaN(pa));
