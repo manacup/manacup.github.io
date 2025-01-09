c93c75bb1134b8dcd8a7aec1eadca53136fac4f1
manacup <91571517+manacup@users.noreply.github.com>
2025-01-09 10:12:27 +0100

    j

--- angular/js/controllers/detallJugadorCtrl.js
+++ angular/js/controllers/detallJugadorCtrl.js
@@ -4,7 +4,7 @@ app.controller("detallJugadorCtrl", function ($scope, $rootScope, $routeParams) 
   $rootScope.pagina = $scope.jugadors[vm.id].Nom;
   //$rootScope.$on('dadesRecuperades', function() {
   console.log('iniciant dades del jugador');
-  $scope.jugador = $scope.jugadors[vm.id]
+  $scope.resumJugador = $scope.jugadors[vm.id]
   $scope.partidesJugador = Object.values($scope.aparellaments).filter(ap => ap.Jugador1.ID == vm.id || ap.Jugador2.ID == vm.id)
   //  });
 });
