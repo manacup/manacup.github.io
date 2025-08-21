b322305a99d8a4dd4f461d38181a5bf2e6cc4d7e
manacup <91571517+manacup@users.noreply.github.com>
2025-08-21 11:16:53 +0200

    Update templates.js

--- xampions/2025/templates.js
+++ xampions/2025/templates.js
@@ -702,7 +702,7 @@ function renderDetallPartida(partida) {
   }</div>
         </blockquote>
         ${
-          partida.Estat == "Ronda tancada"
+          partida.Estat == "Ronda tancada" ||jugadorDesat.Nom=="Antoni Llull" || jugadorDesat.Nom=="Antoni Riera"
             ? ""
             : `<button type="button" class="btn btn-primary mb-4" onclick="loadContent(['formulari',${partida.ID}]);updateHistory(['formulari',${partida.ID}]);">Edita</button>`
         }      
@@ -2062,3 +2062,4 @@ function ExcelDateToJSDate(serial, utc) {
   }
   //;
 }
+
